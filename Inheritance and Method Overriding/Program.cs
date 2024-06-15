9bc79fed85f00c2d039e14302905961946e0a156
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-15 19:31:32 +0000

    Start the Inheritance and Overriding implementation in C#.

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Shapes Program! \n");
+        }
+    }
+}
