49ec5cdaad1206804e45dc56b27c531dd73dcb6e
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-15 21:45:43 +0000

    Complete Animal class.

---
+++ Inheritance and Method Overriding/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AnimalProgram
+{
+    class Animal
+    {
+        //Creates a make sound method
+        internal protected void MakeSound()
+        {
+            Console.WriteLine("Moo");
+        }
+    }
+}
--- Inheritance and Method Overriding/Program.cs
+++ Inheritance and Method Overriding/Program.cs
@@ -1,13 +1,24 @@
 ﻿
 using System;
 
-namespace Csharp
+//Imports the Animal Program namespace so the class with its methods, properties and fields can be accessed
+using AnimalProgram;
+
+namespace MainProgram
 {
     class Program
     {
+        //Creates the main method
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Shapes Program! \n");
+
+            //Animal class instantiation
+            Animal animal = new Animal();
+            Console.WriteLine("Generic sound of a random animal");
+            //Calls the Make Sound method in the Animal class
+            animal.MakeSound();
+
         }
     }
 }
