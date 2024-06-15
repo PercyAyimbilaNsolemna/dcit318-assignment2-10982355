0ea57016e6381aface84df85739ed4fd5f22cb3b
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-15 22:27:47 +0000

    Complete Shape abstract class.

---
+++ Abstract Classes And Methods/Shape.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShapesProgram
+{
+    //Creates an abstract class Shape
+    abstract class Shape
+    {
+        //Creates an abstract method
+        internal protected abstract float GetArea();
+    }
+}
